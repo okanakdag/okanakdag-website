4f3bf80d93c75f74470fedf2805d29c6dfb1e91b
Okan Akdag <mertakdag08@gmail.com>
2025-04-03 04:45:00 +0300

    Updated ProjectCard styling

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,26 +1,17 @@
 export default function ProjectCard({ title, description, tech, link }) {
     return (
-      <div className="border border-white/10 rounded-md p-4 bg-gray-800 hover:bg-gray-700 transition">
-        <h3 className="text-lg font-bold text-white">{title}</h3>
-  
-        <p className="text-gray-400 mt-1">{description}</p>
-  
+      <div className="border border-white/5 rounded-md p-4 hover:bg-gray-800/50">
+        <h3 className="text-m font-semibold text-white">{title}</h3>
+
+        <p className="mt-1">{description}</p>
+
         <ul className="flex flex-wrap gap-2 text-sm text-gray-300 mt-3">
           {tech.map((item) => (
-            <li key={item} className="bg-white/10 px-2 py-1 rounded">
+            <li key={item} className="bg-green-500/10 text-green-50/80 px-2 py-1 rounded font-mono">
               {item}
             </li>
           ))}
         </ul>
-  
-        <a
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-block mt-3 text-sm text-sky-400 hover:underline"
-        >
-          View Project →
-        </a>
       </div>
     );
-  }
+  } 
