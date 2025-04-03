94fc80c86d597b5158172edb674915ac9c425f78
Okan Akdag <mertakdag08@gmail.com>
2025-04-03 06:44:10 +0300

    Added link style to ProjectCard

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,17 +1,35 @@
 export default function ProjectCard({ title, description, tech, link }) {
-    return (
-      <div className="border border-white/5 rounded-md p-4 hover:bg-gray-800/50">
-        <h3 className="text-m font-semibold text-white">{title}</h3>
+  return (
+    <div className="border border-white/5 rounded-md p-4 hover:bg-gray-800/50">
 
-        <p className="mt-1">{description}</p>
+      <div className="flex flex-col sm:flex-row sm:justify-between sm:items-start gap-2">
+        <span className="text-m font-semibold text-white">{title}</span>
 
-        <ul className="flex flex-wrap gap-2 text-sm text-gray-300 mt-3">
-          {tech.map((item) => (
-            <li key={item} className="bg-green-500/10 text-green-50/80 px-2 py-1 rounded font-mono">
-              {item}
-            </li>
-          ))}
-        </ul>
+        {/* View link if exists */}
+        {link && (
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-sm hover:underline break-all"
+          >
+            {link} →
+          </a>
+        )}
       </div>
-    );
-  } 
+
+
+      <p className="mt-1">{description}</p>
+
+      <ul className="flex flex-wrap gap-2 text-sm text-gray-300 mt-3">
+        {tech.map((item) => (
+          <li key={item} className="bg-green-500/10 text-green-50/80 px-2 py-1 rounded font-mono">
+            {item}
+          </li>
+        ))}
+      </ul>
+
+
+    </div>
+  );
+}
